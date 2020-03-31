346c231f62b2f2a5855eba11ac5b2d2b2e82d5a6
Marcio Lacerda <marcioalacerda2201@gmail.com>
2020-03-31 15:52:13 -0300

    criação do tipo Enumerado OrderStatus (declaração das constantes), criação da classe Order (atributos, construtores, gatters e setters, método toString) e Desenvolvimento da classe do programa

---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package entities.enums;
+
+public enum OrderStatus {
+	
+	PENDING_PAYMENT,  //AGUARDANDO PAGAMENTO
+	PROVESSING,       //PROCESSANDO
+	SHIPPED,          //ENVIADO
+	DELIVERED;        //ENTREGUE
+
+}
